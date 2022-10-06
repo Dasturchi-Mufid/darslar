136a4e546feed3aba97bd0e0abb0c68059b42705
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-10-07 03:19:32 +0500

    6-dars Arifmetik operatorlar

---
+++ 6-dars.py
@@ -0,0 +1,76 @@
+# Arifmetik operatorlar
+
+m = 27
+n = 27
+
+# print(m, n, sep='\n')
+
+# print('m + n =',m + n)
+
+# print(f'm - n = {m - n}')
+
+# print(f'm * n = {m * n}')
+
+# print(f'm / n = {m / n}')
+
+# print(f'm % n = {m % n}')
+
+# print(f'm // n = {m // n}')
+
+# print(f'm ** n = {m ** n}')
+
+# Taqqoslash operatorlari
+
+# print(m, n, sep='\n')
+
+# print(f'm > n - {m > n}')
+# print(f'm < n - {m < n}')
+# print(f'm == n - {m == n}')
+# print(f'm != n - {m != n}')
+# print(f'm <= n - {m <= n}')
+
+# Mantiqiy operatorlar
+
+t = True
+f = False
+
+# print(f't = {int(t)} f = {int(f)}')
+# print(f't and f = {t and f and t}')
+# print(f't or f = {t or f}')
+# print(not t)
+# print(not f)
+
+# # Boshqacharoq ko`rinishi
+
+# print(f't = {t} f = {f}')
+# print(f't and f = {t & f}')
+# print(f't or f = {t | f}')
+
+# print(n)
+# n = n + 5
+# print(n)
+
+# print(n)
+# n **= 5
+# print(n)
+
+# -,/,%,//,**, ham shu bilan bir xil
+
+# Maxsus operatorlar
+
+# is, is not operatorlari 
+
+# m1 = 5
+# n1 = 5
+# m2 = 'Salom'
+# n2 = 'Salom'
+m3 = [1,2,3]
+# n3 = [1,2,3]
+# print(m1 is not n1)
+# print(m2 is n2)
+# print(m3 is n3)
+
+# in, in not operatorlari
+
+m = 'Dasturchi'
+print(5 in m3)
