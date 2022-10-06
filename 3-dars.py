a2fed6f824886b801a36ca04a75acd4d65a6340f
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-10-07 03:04:56 +0500

    3-dars O`zgaruvchilar

---
+++ 3-dars.py
@@ -0,0 +1,98 @@
+# # O`zgaruvchilar
+
+# m = 27 # int
+# n = 1.3 # float
+# s = 'Dasturchi_mufid' #string
+# a = "Dasturchi_mufid" #string
+# t = True
+# f = False
+# b = None
+
+# # Tipga o`tkazish
+# m = str(m)
+# n = str(n)
+# # int(m)
+# m = float(m)
+
+# # print(type(m))
+# print(m)
+
+# # O`zgaruvchi sifatida ishlatib bo`lmaydiganlar
+
+# False = 13
+# True = 27
+# global = 'Dasturchi mufid'
+# def = 'Obunachilar hammangizga rahmat'
+
+# print(False)
+
+# # O`zgaruvchilarni tasvirlash
+
+# m = 13
+# m1 = 27
+# M = 9
+# ism_familiya = 'Ahmadaliyev Shahboz'
+
+# print(ism_familiya)
+
+# 1m = 15
+# m! = 25
+# n# = 15
+
+# print(1m)
+
+# name = 'Ahmadaliyev Shahboz'
+# Name = 'Ahmadaliyev Diyorbek'
+
+# print(Name)
+
+# m = 'Shahboz' # bunday emas
+# name = 'Shahboz' # yoki
+# ism = 'Diyorbek'
+
+# print(ism)
+
+# # Uzun bo`lib ketganlarni keyingi qatorga ko`chirish
+
+# matn = 'Do`stlar hammaga salom,
+#     ahvollaringiz yaxshimi?'
+
+# matn = 'Do`stlar hammaga salom,\
+#  ahvollaringiz yaxshimi?'
+
+# print(matn)
+
+# sonlar = 5 + 6 + 9 + 8 +
+# 4 - 88 + 32
+
+# sonlar = (5 + 6 + 9 + 8 +
+# 4 - 88 + 32)
+
+# print(sonlar)
+
+# meva = ['olma','anor',
+# 'nok','banan','kivi']
+
+# print(meva)
+
+# raqamlar = {'one','two',
+# 'three','four'}
+# numbers = {3,5,6,8,4,2,5,6,0,1,2}
+
+# print(numbers)
+
+# # 2-3 qatorni bir qatorga kiritish
+# a = 1; b = 2; c = 3
+# a, b, c = 1, 2, 3
+
+# print(a)
+# print(b)
+# print(c)
+
+# x = y = z = 'test'
+
+# print(x)
+# print(y)
+# print(z)
+
+# m = '27'
