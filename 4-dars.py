3b21bafaf9e768c91dd477a5cc99d07c7dd5564c
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-10-07 03:11:51 +0500

    4-dars print() funksiyasi haqida

---
+++ 4-dars.py
@@ -0,0 +1,49 @@
+# print funkasiyasi haqida
+
+# print('Bu funksiya ekranga ma\'lumot chiqaradi')
+
+
+
+# m = 13
+# n = 27
+# name = 'Shahboz'
+# print('m ning qiymati ',m)
+# print('n ning qiymati ', n)
+# print(m,n,name)
+
+# print(*objects,sep=' ', end='\n', file=sys.stdout, flush=False)
+
+# print(m,n,sep='+')
+
+# print(m)
+# print(n)
+
+# print(m,end='')
+# print(n)
+
+# Formatlash
+
+# print('m ning qiymati {} va n ning qiymati {}'.format(m,n))
+# print('m ning qiymati {0} va n ning qiymati {1}'.format(m,n))
+# print('m ning qiymati {a} va n ning qiymati {b}'.format(a = m,b = n))
+# print(f'm ning qiymati {m}')
+
+# print('Man {} hamda {}ni yaxshi ko`raman'.format('dasturlash','sayohat'))
+
+# s = 3.1415
+
+# print('PI ning qiymati %0.4f' %s )
+# print('PI ning qiymati %5.3f' %s )
+
+# print(f"4 ni 3 ga bo`lsak {4/3:0.2f}")
+# print(f"4 ni 3 ga bo`lsak {4/3:1.2f}")
+# print(f"4 ni 3 ga bo`lsak {4/3:2.2f}")
+# print(f"4 ni 3 ga bo`lsak {4/3:3.2f}")
+# print(f"4 ni 3 ga bo`lsak {4/3:4.2f}")
+# print(f"4 ni 3 ga bo`lsak {4/3:5.2f}")
+# print(f"4 ni 3 ga bo`lsak {4/3:6.2f}")
+# print(f"4 ni 3 ga bo`lsak {4/3:7.2f}")
+# print(f"4 ni 3 ga bo`lsak {4/3:8.2f}")
+# print(f"4 ni 3 ga bo`lsak {4/3:9.2f}")
+# print(f"4 ni 3 ga bo`lsak {4/3:10.2f}")
+
