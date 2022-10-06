6c173965361c66f02601e7ca2833f8eec9165ba6
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-10-07 03:16:11 +0500

    5-dars Kiritish funksiyasi input()

---
+++ 5-dars.py
@@ -0,0 +1,28 @@
+# Kiritish funksiyasi (input())
+
+# input('Biron narsa kiriting: ')
+
+# num1 = int(input('Biron son kiriting: '))
+# num2 = input()
+
+# print(num1,num2)
+# print(type(num1))
+
+# name1 = input('Ismingizni kiriting: ')
+
+# print(f'Assalomu alaykum, {name1}')
+
+# eval() funksiyasi haqida
+
+# code = input('Code ni kiriting: ')
+# print(eval(code))
+
+# import haqida
+
+# import math
+
+# print(math.pi)
+
+# from math import pi
+
+# print(pi)
