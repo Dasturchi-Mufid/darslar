b8da7783a6b7bc62332925b243a2e0f08866e5d3
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-10-07 07:55:45 +0500

    For sikli

---
+++ 8-dars.py
@@ -0,0 +1,61 @@
+# for sikli
+
+# range() funksiyasi haqida
+
+# n = range(10)
+# print(n)
+
+# n = list(range(10))
+# print(n)
+
+# n = list(range(2,8))
+# print(n)
+
+# n = list(range(2,21,3))
+# print(n)
+
+m = 'dasturchi_mufid'
+# print(m * 5)
+
+# for i in range(10):
+#     print(i)
+#     print(m)
+
+# # for da o`zgaruvchi i
+
+# for i in m:
+#     print(i)
+
+# # for yordamida listdagi raqamlar yig`indisini hisoblash
+
+# nums = [54,45,85,41,23,52,68,98]
+# summa = 0
+
+# for i in nums:
+#     summa += i
+
+# print(summa)
+# print(sum(nums))
+
+# list dagi so`zlarni ekranga chiqarish
+
+# meva = ['olma','anor','nok']
+
+# for i in meva:
+#     print(f'Man {i}ni yaxshi ko`raman.')
+
+# for with else
+
+# raqams = [0,13,27,50]
+
+# for i in raqams:
+#     print(i)
+# else:
+#     print('Listda raqamlar yo`q yoki tugab qoldi.')
+
+# for siklida if dan foydalanish
+
+# for i in range(10):
+#     if i % 2 == 0:
+#         print(i)
+print(list(range(0,10,2)))
