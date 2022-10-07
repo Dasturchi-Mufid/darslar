d16d40ee691350711caec75094ce2f35be8d9d4c
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-10-07 07:54:51 +0500

    Shart operatori if

---
+++ 7-dars.py
@@ -0,0 +1,52 @@
+# if operatori haqida
+
+m = -5
+# n = int(input('Biron son kiriting: '))
+
+# if m > 0:
+#     print(f'{m} musbat son')
+# print(m)
+
+# if m > 0:
+#     print(f'{m} musbat son')
+# else:
+#     print(f'{m} manfiy son')
+# print(m)
+
+# if n > 0:
+#     print(f'{n} musbat son')
+# elif n == 0:
+#     print(f'{n} nolga teng.')
+# else:
+#     print(f'{n} manfiy son')
+# print(n)
+
+# temp = -10
+
+# if...elif...else umumiy
+
+# if temp > 5:
+#     print('Bugun havo ochiq. Yengilroq kiyinavering.')
+# elif temp <= 5 and temp >= -5:
+#     print('Issiq kiyimingizni olvoling, havo sovub ketishi mumkin.')
+# else:
+#     print('Bugun havo sovuq bo`ladi.'
+#     ' Issiqroq kiyining. \nKasal bo`lishingiz mumkin.')
+
+# if... ichida if...
+
+# if m >= 0:    
+#     if m == 0:
+#         print(f'{m} nolga teng.')
+#     else:
+#         print(f'{m} musbat son')
+# else:
+#     print(f'{m} manfiy son')
+# print(m)
+
+if False:
+    print('salom')
+elif True:
+    print('qalaysiz?')
+
+
