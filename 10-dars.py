afbe7bcf4a1b10f54de39108eb734663580bf4f4
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-10-07 08:01:43 +0500

    Break va Continue

---
+++ 10-dars.py
@@ -0,0 +1,56 @@
+# break haqida
+
+# txt = input('Son kiritmang: ')
+# for i in txt:
+#     if i == '0':
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+#     if i == '1':
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+#     if i == '2':
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+#     if i == '3':
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+#     if i == '4':
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+#     if i == '5':
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+#     if i == '6':
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+#     if i == '7':
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+#     if i == '8':
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+#     if i == '9':
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+
+# for i in txt:
+#     if i.isnumeric():
+#         print(f'Sizning matningizda {i} soni mavjud')
+#         break
+    
+
+string = input('Biron narsa kiriting:')
+
+for i in string:
+    if i == 'a':
+        continue
+    if i == 'e':
+        continue
+    if i == 'i':
+        continue
+    if i == 'o':
+        continue
+    if i == 'u':
+        continue
+    print(i)
+print('Sikl tugadi.')
