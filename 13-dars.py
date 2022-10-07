640e3750c4179e6a040fc1319d382dcf258827d4
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-10-07 08:05:42 +0500

    Funksiyalar (davomi)

---
+++ 13-dars.py
@@ -0,0 +1,34 @@
+# def greet(name, msg):
+#     '''Bu funksiya name o`zgaruvchisiga kiritilgan
+#     insonga msg o`zgaruvchisiga kiritilgan habarni 
+#     qo`shib yozadi.'''
+#     print(f'Salom, {name} {msg}')
+
+# greet('Shahboz','Qalesiz?')
+
+# greet('Shahboz')
+
+# greet()
+
+# greet('Nima qilyapsiz?','Shahboz')
+
+# def greet(name, msg = 'Hayrli tong!'):
+#     print(f'Salom, {name} {msg}')
+    
+# greet('Shahboz','Qalesiz?')
+
+# greet('Shahboz')
+
+# def greet(msg = 'Hayrli tong', name):
+#     print(f'Salom, {name} {msg}')
+
+# greet(name = 'Mashhur',msg = 'Qalesiz?')
+# greet(msg = 'Qalesiz?', name = 'Shohruh')
+
+# def greets(*names):
+#     for name in names:
+#         print(f'Salom, {name}')
+
+# greets('Shahboz','Sobir','Mashhur')
+
+
