b7bf95b76f0840c500ade54fdcca3f70b7615736
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-10-07 07:57:15 +0500

    While sikli

---
+++ 9-dars.py
@@ -0,0 +1,37 @@
+# while sikli
+
+# Shart
+# 1 dan 100 gacha bo`lgan sonlar yig`indisini hisoblang.
+
+# n = 100
+# n = int(input('Nechigacha bo`lgan sonlar yig`indisini hisoblaylik: '))
+
+# i = 1
+# summa = 0
+
+# while i <= n:
+#     summa += i
+#     i += 1
+
+# print(f'1 dan 100 gacha bo`lgan sonlar yig`indisi {summa} ga teng')
+
+# counter = 0
+
+# while counter < 3:
+#     print('Sikl ichida')
+#     counter += 1
+#     if counter == 2:
+#         break
+# else:
+#     print('else ni ichida')
+
+m = -1
+
+while m <= 0:
+    if m < 0:
+        print(f'Bu {m} manfiy son')
+    if m == 0:
+        print(f'{0} bu nolga teng')
+    m = float(input('Musbat son kiriting:'))
+
+print('E bo`larkanu!')
