b7f565fb8a0ed2ac303d6dff4f6fbbd6d44d8964
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-12-13 15:28:51 +0500

    Listlar

---
+++ 17-dars.py
@@ -0,0 +1,73 @@
+# Pythonda listlar
+
+m = ['python','dasturchi','mufid'] # Matndan iborat list
+n = [1,2.4,85,65,22] # Sonlardan iborat list
+l = [m,11,23,5.4,'hammaga','salom',[1,2,3]] # Aralash list
+s = []
+
+# Listlar
+
+
+# print(m)
+# print(n)
+# print(l)
+# print(s)
+
+# Indexlar
+
+# print(m[0])
+# print(n[0])
+# print(l[0])
+# print(s[0])
+
+# txt = 'salom'
+
+# print(txt)
+# print(txt[0])
+
+# print(l)
+# print(l[4][3])
+
+# print(m)
+# print(m[-1])
+
+# print(l)
+# print(l[-5])
+
+# Oraliqlar
+
+my_list = ['a','b','c','d','e','f','g']
+
+# print(my_list)
+# print(my_list[2:5])
+# print(my_list[3:])
+# print(my_list[:])
+# print(my_list[2::2])
+# print(my_list[::-1])
+
+# Listga element qo`shish va o`zgartirish kiritish
+
+# juft = [2,4,6,8,10]
+# print(juft)
+
+# juft[3] = 0
+# print(juft)
+
+# juft[1:4] = [3,5,7,9]
+# print(juft)
+
+
+# toq = [1,7,5]
+# print(toq)
+
+# toq.append(70)
+# toq.append([1,3,5,7,9])
+
+# print(toq)
+# print(toq[3][1])
+
+# toq.extend([7,9])
+# print(toq)
+
+# print(toq + juft)
+# print(['mufid','dasturchi']*3)
