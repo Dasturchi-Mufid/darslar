3de67659abc3d429765327018b78b3a0cbe88805
Dasturchi-Mufid <104354937+Dasturchi-Mufid@users.noreply.github.com>
2022-12-27 12:29:01 +0500

    Listlar davomi

---
+++ 18-dars.py
@@ -0,0 +1,65 @@
+# nums = [2,9]
+# print(nums)
+
+# nums.insert(1,3)
+# print(nums)
+
+# nums[2:2] = [5,7]
+# print(nums)
+
+# Listdan element o`chirish
+
+# my_list = ['a','b','c','d','e','f','g','h']
+# print(my_list)
+
+# del my_list[2]
+# print(my_list)
+
+# del my_list[1:5]
+# print(my_list)
+
+# del my_list
+# print(my_list)
+
+# new_list = ['m','n','l','o','p','q','r','s']
+# print(new_list)
+
+# new_list.remove('o')
+# print(new_list)
+
+# print(new_list.pop(4))
+# m = new_list.pop(1)
+# print(new_list)
+# print(m)
+
+# n = new_list.pop()
+# print(n)
+
+# new_list.clear()
+# print(new_list)
+
+# lists = [4,5,8,6,2,5,9,5,3,6]
+# print(lists.index(6))
+
+# Listlar bilan qiziqarli kodlar
+
+# daraja = [2 ** i for i in range(10)]
+# print(daraja)
+
+# daraja = []
+
+# for i in range(10):
+#     daraja.append(2 ** i)
+
+# print(daraja)
+
+# my_list = ['s','m','n','d','l','f']
+
+# print('m' in my_list)
+# print('q' in my_list)
+# print('a' not in my_list)
+
+# mevalar = ['olma','anor','nok']
+
+# for meva in mevalar:
+#     print(f'Man {meva}ni yaxshi ko`raman.')
