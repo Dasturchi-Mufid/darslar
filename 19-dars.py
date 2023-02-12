e408c8dc9f64d54811016f3614ac55d6450f67ec
Dasturchi-Mufid <shahbozbekahmadaliyev@gmail.com>
2023-02-12 13:42:48 +0500

    Tuple

---
+++ 19-dars.py
@@ -0,0 +1,69 @@
+# Tuple
+
+# my_tuple = () # Empty tuple
+# print(my_tuple)
+
+# nums_tuple = (1, 2.4, 3)
+# nums_tuple[0] = 5
+# print(nums_tuple)
+
+# n = 'salom'
+# my_tuple = (1, 'Hello', 3,n)
+# print(my_tuple)
+
+# my_tuple = ('string',[1,2,3],(1,2,31))
+# print(my_tuple[2][2])
+
+# var1 = ("Hello") # string
+# var2 = ("Hello",) # tuple
+# print(type(var1))
+# print(type(var2))
+
+# var1 = ("hello")
+# print(type(var1))  
+
+# var2 = ("hello",)
+# print(type(var2))  
+
+# var3 = "hello",2
+# print(type(var3))
+
+# letters = ("p", "r", "o", "l", "r", "b", "m", "m", "z")
+
+# print(letters)
+# print(letters[0])  
+# print(letters[5])  
+
+# letters = ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
+
+# print(letters[-1])
+# print(letters[-3])
+
+# my_tuple = ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'u', 'z')
+
+# print(my_tuple[1:4])
+
+# print(my_tuple[:-7])
+
+# print(my_tuple[7:])
+
+# print(my_tuple[:])
+
+# my_tuple = ('a', 'p', 'p', 'l', 'e',)
+
+# print(my_tuple.count('p'))
+# print(my_tuple.index('l'))
+
+# tuple methods
+
+# languages = ('Python', 'Swift', 'C++')
+
+# # iterating through the tuple
+# for language in languages:
+#     print(f'Man {language} dasturlash tilini o`rganmoqchiman.')
+
+
+# languages = ('Python', 'Swift', 'C++')
+
+# print('C' in languages)
+# print('Python' in languages)
