63b50eaa9e411814919fd6202bc2d3a4b5b39475
Dasturchi-Mufid <shahbozbekahmadaliyev@gmail.com>
2023-02-27 14:50:13 +0500

    Stringlar haqida

---
+++ 20-dars.py
@@ -0,0 +1,72 @@
+# String haqida
+
+# string1 = "Dasturchi Mufid"
+# string2 = 'Dasturchi Mufid'
+# print(string2)
+
+# Indexing
+
+# txt = 'hammaga salom'
+
+# print(txt[2])
+# print(txt[-5])
+# print(txt[2:-4])
+
+# Errors
+
+# matn = 'Hello world!'
+# matn[0] = 'S'
+# print(matn)
+
+# matn = 'Hello world!'
+# matn = 'Salom world!'
+
+# print(matn)
+
+# Multiline String
+
+# msg = '''
+#     Bu yerga hohlagancha yozishingiz mumkin.
+#     Bu ishni bajarishimiz oson.
+# '''
+# print(msg)
+
+# msg = """Bu yerga ham"""
+
+# print(msg)
+
+# Checking
+
+# str1 = "Hello, world!"
+# str2 = "I love Python."
+# str3 = 'Hello, world!'
+
+# print(str1 == str2)
+
+# print(str1 == str3)
+
+# Adding
+
+# greet = "Hello,"
+# name = "Jack"
+
+# result = greet + ' ' + name
+# print(result)
+
+# Using loop
+
+# greet = 'Hello'
+
+# for letter in greet:
+#     print(letter)
+
+# Some functions
+
+# greet = 'Hello'
+
+# print(greet.replace('H','S'))
+
+# Checking
+
+# print('a' in 'program') 
+# print('at' not in 'battle') 
