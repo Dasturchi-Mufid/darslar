be110a28627b0322c5724dcaf169b05d8ef11d88
Dasturchi-Mufid <shahbozbekahmadaliyev@gmail.com>
2023-04-28 17:43:42 +0500

    Setlar

---
+++ 21-dars.py
@@ -0,0 +1,59 @@
+# Set 
+
+# student_id = {112, 114, 116, 118, 115}
+# print('Student ID:', student_id)
+
+# vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+# print('Vowel Letters:', vowel_letters)
+
+# mixed_set = {'Hello', 101, -2.1, 'Bye'}
+# print('Set of mixed data types:', mixed_set)
+
+# empty_set = set()
+
+# empty_dictionary = { }
+
+# print('Data type of empty_set:', type(empty_set))
+
+# print('Data type of empty_dictionary', type(empty_dictionary))
+
+# numbers = {2, 4, 6, 6, 2, 8}
+# print(numbers) 
+
+# # Adding 
+
+# numbers = {21, 34, 54, 12}
+
+# print('Initial Set:',numbers)
+
+# numbers.add(32)
+
+# print('Updated Set:', numbers) 
+
+# # Update
+
+# companies = {'Lacoste', 'Ralph Lauren'}
+# tech_companies = ['Apple', 'Google', 'Apple']
+
+# companies.update(tech_companies)
+
+# print(companies)
+
+# Removing
+
+# languages = {'Swift', 'Java', 'Python'}
+
+# print('Initial Set:',languages)
+
+# removedValue = languages.discard('Java')
+
+# print(removedValue)
+# print('Set after remove():', languages)
+
+# Looping
+
+fruits = {"Apple", "Peach", "Mango"}
+
+for fruit in fruits: 
+    print(fruit)
+
