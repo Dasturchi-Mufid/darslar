c5dd04d7b22ca3da3f7e5dc5f4f2db74c52c03a5
Dasturchi-Mufid <shahbozbekahmadaliyev@gmail.com>
2023-09-21 15:23:34 +0500

    Fayllar bilan ishlash

---
+++ 23-dars.py
@@ -0,0 +1,55 @@
+# Filelar bilan ishlash
+
+# f = open("matn.txt")
+# print(f)
+# print(f.read())
+# f.close()
+
+# f = open("matn.txt", "r")
+# print(f.read(6))
+# print(f.read(10))
+# f.close()
+
+# f = open("matn.txt", "r")
+# print(f.read(10))
+# f.close()
+
+# f = open("matn2.txt", "w")
+# f.write('Salom\n')
+# f.write('\nAssalomu alaykum')
+# f.close()
+
+# f = open("matn.txt", 'a')
+# f.write('\nSalom')
+# f.close()
+
+# try:
+#     file1 = open("matn2.txt", "r")
+#     read_content = file1.read()
+#     print(read_content)
+# except:
+#     pass
+# finally:
+#     # close the file
+#     file1.close()
+# file1 = open('matn.txt','r')
+
+# with open("matn.txt", "r") as file1:
+#     read_content = file1.read()
+#     print(read_content)
+
+# with open('matn3.txt', 'w') as file2:
+
+#     file2.write('Programming is Fun.')
+#     file2.write(' Dasturchi Mufid\'s videos are for beginners')
+
+# with open('matn3.txt','a') as f:
+#     f.write('\nYana bir qator.')
+
+# with open('matn3.txt','r') as f:
+#     lines = f.readlines()
+#     print(lines[1])
+
+with open('js.txt','w') as f:
+    lines = ['JS is also awesome\n','JS is my second programming language']
+    f.writelines(lines)
