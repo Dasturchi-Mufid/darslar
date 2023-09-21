f751c1bdb5fa22209008fd7c949fe4d583dab501
Dasturchi-Mufid <shahbozbekahmadaliyev@gmail.com>
2023-09-21 15:26:30 +0500

    CMD ni python code yordamida ishlatish

---
+++ 24-dars.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+
+#1 print(os.getcwd())
+
+#2 os.chdir('m:\\Django')
+
+# print(os.getcwd())
+
+# list all sub-directories
+#3 print(os.listdir())
+
+# print(os.listdir('c:\\'))
+
+# os.mkdir('yangi')
+
+# os.getcwd() # because file directory is other
+
+# print(os.listdir())
+
+#5 rename a directory
+# os.rename('yangi','new_one')
+
+# os.listdir()
+
+# delete "myfile.txt" file
+# os.remove("myfile.txt")
+
+# delete the empty directory "yangi"
+# os.rmdir("yangi") 
+
+
+# delete "yangi" directory and all of its contents
+# shutil.rmtree("yangi")
